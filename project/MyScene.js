29f978af26b1f0d1c3ad1e5bcfa29a703ca6b54b
angel <up202108767@up.pt>
2024-04-08 10:05:35 +0100

    added sphere

--- project/MyScene.js
+++ project/MyScene.js
@@ -1,5 +1,6 @@
 import { CGFscene, CGFcamera, CGFaxis, CGFappearance, CGFshader, CGFtexture } from "../lib/CGF.js";
 import { MyPlane } from "./MyPlane.js";
+import { MySphere } from "./MySphere.js";
 
 /**
  * MyScene
@@ -7,89 +8,90 @@ import { MyPlane } from "./MyPlane.js";
  */
 export class MyScene extends CGFscene {
 
-    constructor() {
-      super();
-    }
+  constructor() {
+    super();
+  }
 
-    init(application) {
-      super.init(application);
-      
-      this.initCameras();
-      this.initLights();
+  init(application) {
+    super.init(application);
 
-      //Background color
-      this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    this.initCameras();
+    this.initLights();
 
-      this.gl.clearDepth(100.0);
-      this.gl.enable(this.gl.DEPTH_TEST);
-      this.gl.enable(this.gl.CULL_FACE);
-      this.gl.depthFunc(this.gl.LEQUAL);
+    //Background color
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-      //Initialize scene objects
-      this.axis = new CGFaxis(this);
-      this.plane = new MyPlane(this,30);
+    this.gl.clearDepth(100.0);
+    this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.enable(this.gl.CULL_FACE);
+    this.gl.depthFunc(this.gl.LEQUAL);
 
-      //Objects connected to MyInterface
-      this.displayAxis = true;
-      this.scaleFactor = 1;
+    //Initialize scene objects
+    this.axis = new CGFaxis(this);
+    this.plane = new MyPlane(this, 30);
+    this.sphere = new MySphere(this, 100, 100);
 
-      this.enableTextures(true);
+    //Objects connected to MyInterface
+    this.displayAxis = true;
+    this.scaleFactor = 1;
 
-      this.texture = new CGFtexture(this, "images/terrain.jpg");
-      this.appearance = new CGFappearance(this);
-      this.appearance.setTexture(this.texture);
-      this.appearance.setTextureWrap('REPEAT', 'REPEAT');
+    this.enableTextures(true);
 
-    }
+    this.texture = new CGFtexture(this, "images/terrain.jpg");
+    this.appearance = new CGFappearance(this);
+    this.appearance.setTexture(this.texture);
+    this.appearance.setTextureWrap('REPEAT', 'REPEAT');
 
-    initLights() {
-      this.lights[0].setPosition(15, 0, 5, 1);
-      this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-      this.lights[0].enable();
-      this.lights[0].update();
-    }
+  }
 
-    initCameras() {
-      this.camera = new CGFcamera(
-        1.0,
-        0.1,
-        1000,
-        vec3.fromValues(50, 10, 15),
-        vec3.fromValues(0, 0, 0)
-      );
-    }
+  initLights() {
+    this.lights[0].setPosition(15, 0, 5, 1);
+    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+    this.lights[0].enable();
+    this.lights[0].update();
+  }
 
-    setDefaultAppearance() {
-      this.setAmbient(0.2, 0.4, 0.8, 1.0);
-      this.setDiffuse(0.2, 0.4, 0.8, 1.0);
-      this.setSpecular(0.2, 0.4, 0.8, 1.0);
-      this.setShininess(10.0);
-    }
+  initCameras() {
+    this.camera = new CGFcamera(
+      1.0,
+      0.1,
+      1000,
+      vec3.fromValues(50, 10, 15),
+      vec3.fromValues(0, 0, 0)
+    );
+  }
 
-    display() {
-      // ---- BEGIN Background, camera and axis setup
-      // Clear image and depth buffer everytime we update the scene
-      this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
-      this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-      // Initialize Model-View matrix as identity (no transformation
-      this.updateProjectionMatrix();
-      this.loadIdentity();
-      // Apply transformations corresponding to the camera position relative to the origin
-      this.applyViewMatrix();
+  setDefaultAppearance() {
+    this.setAmbient(0.2, 0.4, 0.8, 1.0);
+    this.setDiffuse(0.2, 0.4, 0.8, 1.0);
+    this.setSpecular(0.2, 0.4, 0.8, 1.0);
+    this.setShininess(10.0);
+  }
 
-      // Draw axis
-      if (this.displayAxis) this.axis.display();
+  display() {
+    // ---- BEGIN Background, camera and axis setup
+    // Clear image and depth buffer everytime we update the scene
+    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    // Initialize Model-View matrix as identity (no transformation
+    this.updateProjectionMatrix();
+    this.loadIdentity();
+    // Apply transformations corresponding to the camera position relative to the origin
+    this.applyViewMatrix();
 
-      // ---- BEGIN Primitive drawing section
+    // Draw axis
+    if (this.displayAxis) this.axis.display();
 
-      this.pushMatrix();
-      this.appearance.apply();
-      this.translate(0,-100,0);
-      this.scale(400,400,400);
-      this.rotate(-Math.PI/2.0,1,0,0);
-      this.plane.display();
-      this.popMatrix();
+    // ---- BEGIN Primitive drawing section
+    this.pushMatrix();
+    this.appearance.apply();
+    this.translate(0, -100, 0);
+    this.scale(400, 400, 400);
+    this.rotate(-Math.PI / 2.0, 1, 0, 0);
+    this.plane.display();
+    this.popMatrix();
+    this.sphere.display();
 
-      // ---- END Primitive drawing section
-    }
+    // ---- END Primitive drawing section
+  }
 }
---
+++ project/MySphere.js
@@ -0,0 +1,53 @@
+import { CGFobject } from '../lib/CGF.js';
+/**
+* MySphere
+* @constructor
+ * @param scene - Reference to MyScene object
+ * @param slices - number of divisions around the Y axis
+ * @param stacks - number of divisions along the Y axis
+*/
+export class MySphere extends CGFobject {
+    constructor(scene, slices, stacks) {
+        super(scene);
+        this.slices = slices;
+        this.stacks = stacks;
+        this.initBuffers();
+    }
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+
+        var ang = 0;
+        var step = 2 * Math.PI / this.slices;
+        var stackStep = Math.PI / this.stacks;
+
+        for (var i = 0; i <= this.slices; i++) {
+            for (var j = 0; j <= this.stacks; j++) {
+                var sin_ang = Math.sin(ang);
+                var cos_ang = Math.cos(ang);
+                var sin_stack = Math.sin(stackStep * j - Math.PI / 2);
+                var cos_stack = Math.cos(stackStep * j - Math.PI / 2);
+
+                var x = cos_ang * cos_stack;
+                var y = sin_ang * cos_stack;
+                var z = sin_stack;
+
+                this.vertices.push(x, y, z);
+                this.normals.push(x, y, z);
+
+                if (i > 0 && j > 0) {
+                    var vertices = this.vertices.length / 3;
+                    this.indices.push(vertices - 1, vertices - this.stacks - 3, vertices - 2);
+                    this.indices.push(vertices - 1, vertices - this.stacks - 2, vertices - this.stacks - 3);
+                }
+            }
+            ang += step;
+        }
+
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+
+        this.initGLBuffers();
+    }
+}
